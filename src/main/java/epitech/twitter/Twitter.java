7218cd58a02e38214b7f44c9ed91fb199fd4a6a7
ELEOUET Gaëtan <gaetan.eleouet@gmail.com>
2021-10-14 15:11:55 +0200

    add persistance

--- src/main/java/epitech/twitter/Twitter.java
+++ src/main/java/epitech/twitter/Twitter.java
@@ -5,12 +5,14 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.core.mapper.reflect.BeanMapper;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import epitech.database.Database;
 import io.javalin.Javalin;
 import io.javalin.http.NotFoundResponse;
 import io.javalin.http.UnauthorizedResponse;
@@ -41,11 +43,28 @@ public class Twitter {
 			this.idUser = idUser;
 			this.idFollowed = idFollowed;
 		}
+
+		public Integer getIdUser() {
+			return idUser;
+		}
+
+		public void setIdUser(Integer idUser) {
+			this.idUser = idUser;
+		}
+
+		public Integer getIdFollowed() {
+			return idFollowed;
+		}
+
+		public void setIdFollowed(Integer idFollowed) {
+			this.idFollowed = idFollowed;
+		}
+
 	}
 
 	public static class Post {
 		public String idPost;
-		public Instant timestamp;
+		public Instant creationDate;
 		public Integer idAuthor;
 		public String message;
 
@@ -55,10 +74,43 @@ public class Twitter {
 		public Post(String idPost, Instant timestamp, Integer idAuthor, String message) {
 			super();
 			this.idPost = idPost;
-			this.timestamp = timestamp;
+			this.creationDate = timestamp;
 			this.idAuthor = idAuthor;
 			this.message = message;
 		}
+
+		public String getIdPost() {
+			return idPost;
+		}
+
+		public void setIdPost(String idPost) {
+			this.idPost = idPost;
+		}
+
+		public Instant getCreationDate() {
+			return creationDate;
+		}
+
+		public void setCreationDate(Instant creationDate) {
+			this.creationDate = creationDate;
+		}
+
+		public Integer getIdAuthor() {
+			return idAuthor;
+		}
+
+		public void setIdAuthor(Integer idAuthor) {
+			this.idAuthor = idAuthor;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+		public void setMessage(String message) {
+			this.message = message;
+		}
+
 	}
 
 	public static class User {
@@ -72,40 +124,53 @@ public class Twitter {
 			this.name = login;
 			this.id = id;
 		}
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
 	}
 
 	public static void main(String[] args) {
 		int port = Integer.parseInt(System.getProperty("port", "9001"));
+		Jdbi jdbi = Database.start("twitter");
+		initDB(jdbi);
 
 		ObjectMapper mapper = new ObjectMapper();
 		mapper.findAndRegisterModules();
-		
+
 		Javalin app = Javalin.create(config -> {
 			config.jsonMapper(new JavalinJackson(mapper));
 		});
 
-		List<User> users = new CopyOnWriteArrayList<>();
-		List<Post> posts = new CopyOnWriteArrayList<>();
-		List<Follow> follows = new CopyOnWriteArrayList<>();
-
-		AtomicInteger idsUsers = new AtomicInteger();
-
 		app.get("/members", ctx -> {
-			ctx.json(users);
+			ctx.json(users(jdbi));
 		});
 		app.get("/register/{login}", ctx -> {
 			String login = ctx.pathParam("login");
-			Optional<User> existing = users.stream().filter(u -> u.name.equals(login)).findAny();
+			Optional<User> existing = users(jdbi).stream().filter(u -> u.name.equals(login)).findAny();
 			if (existing.isPresent()) {
 				ctx.status(400);
 				ctx.result("Already existing");
 			} else {
-				users.add(new User(login, idsUsers.getAndIncrement()));
+				add(jdbi, login);
 			}
 		});
 		app.get("/login/{login}", ctx -> {
 			String login = ctx.pathParam("login");
-			Optional<User> existing = users.stream().filter(u -> u.name.equals(login)).findAny();
+			Optional<User> existing = users(jdbi).stream().filter(u -> u.name.equals(login)).findAny();
 			if (existing.isPresent()) {
 				ctx.cookie("id", "" + existing.get().id);
 			} else {
@@ -127,12 +192,11 @@ public class Twitter {
 		app.post("/postTweet", ctx -> {
 			Integer id = Integer.parseInt(ctx.cookie("id"));
 			Tweet tweet = ctx.bodyAsClass(Tweet.class);
-			Optional<User> user = users.stream().filter(u -> u.id == id).findAny();
+			Optional<User> user = users(jdbi).stream().filter(u -> u.id == id).findAny();
 			if (user.isEmpty()) {
 				throw new UnauthorizedResponse();
 			}
-			Post newPost = new Post(UUID.randomUUID().toString(), Instant.now(), id, tweet.message);
-			posts.add(newPost);
+			Post newPost = add(jdbi, id, tweet.message);
 			System.out.println(user.get().name + "=>" + tweet.message);
 			ctx.json(newPost);
 		});
@@ -140,18 +204,58 @@ public class Twitter {
 			Integer id = Integer.parseInt(ctx.cookie("id"));
 			Integer idUser = ctx.pathParamAsClass("idUser", Integer.class).get();
 			Follow follow = new Follow(id, idUser);
-			follows.add(follow);
+			add(jdbi, follow);
 		});
 		app.get("/timeline", ctx -> {
 			Integer id = Integer.parseInt(ctx.cookie("id"));
-			List<Post> timeline = follows.stream()
-					.filter(f -> f.idUser == id)
-					.peek(f -> System.out.println(f.idUser +" => " + f.idFollowed))
-					.flatMap(f -> posts.stream().filter(p -> p.idAuthor == f.idFollowed)).sorted(Comparator.comparing(p -> p.timestamp))
-					.limit(10).collect(Collectors.toList());
+			List<Post> timeline = follows(jdbi).stream().filter(f -> f.idUser == id).peek(f -> System.out.println(f.idUser + " => " + f.idFollowed)).flatMap(f -> posts(jdbi).stream().filter(p -> p.idAuthor == f.idFollowed))
+					.sorted(Comparator.comparing(p -> p.creationDate)).limit(10).collect(Collectors.toList());
 			ctx.json(timeline);
 		});
 
 		app.start(port);
+	}
+
+	private static void add(Jdbi jdbi, Follow follow) {
+		jdbi.withHandle(h -> h.createUpdate("INSERT INTO follow (idUser, idFollowed) VALUES (:idUser, :idFollowed)").bind("idUser", follow.idUser).bind("idFollowed", follow.idFollowed).execute());
+	}
+
+	private static List<Follow> follows(Jdbi jdbi) {
+		List<Follow> follows = jdbi.withHandle(h -> h.select("SELECT * FROM follow").mapTo(Follow.class).list());
+		return follows;
+	}
+
+	private static Post add(Jdbi jdbi, Integer id, String message) {
+		var uuid = UUID.randomUUID().toString();
+		var creationDate = Instant.now();
+		jdbi.withHandle(h -> h.createUpdate("INSERT INTO post " + " (idPost, idAuthor, message, creationDate) VALUES (:idPost, :idAuthor, :message, :creationDate)").bind("idPost", uuid).bind("idAuthor", id).bind("message", message)
+				.bind("creationDate", creationDate).execute());
+		return new Post(uuid, creationDate, id, message);
+	}
+
+	private static List<Post> posts(Jdbi jdbi) {
+		List<Post> posts = jdbi.withHandle(h -> h.select("SELECT * FROM post").mapTo(Post.class).list());
+		return posts;
+	}
+
+	private static User add(Jdbi jdbi, String name) {
+		return jdbi.withHandle(h -> h.createUpdate("INSERT INTO user (name) VALUES (:name)").bind("name", name).executeAndReturnGeneratedKeys("id").mapTo(User.class).one());
+	}
+
+	private static List<User> users(Jdbi jdbi) {
+		List<User> users = jdbi.withHandle(h -> h.select("SELECT * FROM user").mapTo(User.class).list());
+		return users;
+	}
+
+	private static void initDB(Jdbi jdbi) {
+		jdbi.registerRowMapper(BeanMapper.factory(User.class));
+		jdbi.registerRowMapper(BeanMapper.factory(Post.class));
+		jdbi.registerRowMapper(BeanMapper.factory(Follow.class));
+		jdbi.withHandle(h -> h.execute("CREATE TABLE IF NOT EXISTS post ( " + "   idPost  VARCHAR(64) PRIMARY KEY, " + "   message  VARCHAR(140), " + "   creationdate  TIMESTAMP," + "   idAuthor  NUMBER" + "   ) "));
+
+		jdbi.withHandle(h -> h.execute("CREATE TABLE IF NOT EXISTS follow ( " + "   idUser  NUMBER, " + "   idFollowed  NUMBER " + "   ) "));
+
+		jdbi.withHandle(h -> h.execute("CREATE TABLE IF NOT EXISTS user ( " + "   id  NUMBER PRIMARY KEY AUTO_INCREMENT, " + "   name  VARCHAR(140) " + "   ) "));
+
 	}
 }
